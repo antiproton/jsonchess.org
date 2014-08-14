a003d44dca1282e524f341303153002b81c0b717
Gus <gus@hogg-blake.co.uk>
2014-08-14 18:40:16 +0100

    converts home from md to php

---
+++ home.php
@@ -0,0 +1,19 @@
+<p>
+	<strong>jsonchess</strong> is a protocol for playing chess games over the
+	internet. Clients connect to a server to play games against each other by
+	exchanging messages. These documents describe what messages each end
+	needs to send and listen for, and define some constants to specify how
+	they should behave.
+</p>
+<p>
+	The protocol consists of a core, which must be supported by all jsonchess
+	servers, and several optional modules. Clients can request details of
+	what modules are supported from the server.
+</p>
+<p>
+	To see it in action, go to <a href="http://jsonchess.com">jsonchess.com</a>.
+</p>
+<p>
+	jsonchess is open-source. If you have any feedback or suggestions, feel
+	free to post an issue to <a href="http://github.com/jsonchess/jsonchess">github</a>.
+</p>
