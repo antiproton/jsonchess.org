957c773dd12ae94311905816f77ef344da630607
Gus <gus@hogg-blake.co.uk>
2014-08-15 16:26:02 +0100

    adds function to remove extra whitespace around code tags within <pre>

--- js/cleanupCodeTags.js
+++ js/cleanupCodeTags.js
@@ -1,4 +1,8 @@
 function normaliseIndentation(node) {
+	/*
+	remove any indentation caused by the formatting of the source HTML.
+	*/
+	
 	var lines = node.innerHTML.split("\n");
 	var minIndentationLevel = Infinity;
 	
@@ -26,9 +30,26 @@ function tabsToSpaces(node) {
 	node.innerHTML = node.innerHTML.replace(/\t/g, "   ");
 }
 
-function cleanupCodeTags() {
-	var nodes = [].slice.call(document.querySelectorAll("pre code"));
+function trimTextNodes(pre) {
+	/*
+	<pre>
+		<-- text node with whitespace  --|
+		<code> ... </code>               |-- remove these to get rid of extra whitespace
+		<-- text node with whitespace  --|
+	</pre>
+	*/
 	
-	nodes.forEach(normaliseIndentation);
-	nodes.forEach(tabsToSpaces);
+	if(pre.childNodes.length === 3 && pre.childNodes[1].tagName === "CODE") {
+		pre.removeChild(pre.childNodes[0]);
+		pre.removeChild(pre.childNodes[1]);
+	}
+}
+
+function cleanupCodeTags() {
+	var pre = [].slice.call(document.querySelectorAll("pre"));
+	var code = [].slice.call(document.querySelectorAll("pre code"));
+	
+	code.forEach(normaliseIndentation);
+	code.forEach(tabsToSpaces);
+	pre.forEach(trimTextNodes);
 }
